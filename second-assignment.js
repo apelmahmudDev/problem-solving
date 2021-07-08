7bef3eb76b7938cd5ea88595feaac6a8ee3ca726
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-07-08 10:07:28 +0600

    Problem-6 budget calculator is done

--- second-assignment.js
+++ second-assignment.js
@@ -1,4 +1,4 @@
-// kilometer to meter converter
+// 5. kilometer to meter converter
 
 function kilometerToMeter(kilometer) {
 	// 1 kilometer = 1000 meter
@@ -6,4 +6,22 @@ function kilometerToMeter(kilometer) {
 }
 
 const kilometer = 30;
-console.log(kilometerToMeter(kilometer));
+const meter = kilometerToMeter(kilometer);
+// console.log(meter);
+
+// 6. budgetCalculator
+
+function budgetCalculator(watch, mobile, latptop) {
+	const watchPrice = 50;
+	const mobilePrice = 100;
+	const laptopPrice = 500;
+	const totalPrice =
+		watch * watchPrice + mobile * mobilePrice + latptop * laptopPrice;
+	return totalPrice;
+}
+
+const watch = 2;
+const mobile = 5;
+const latptop = 2;
+const totalPrice = budgetCalculator(watch, mobile, latptop);
+console.log(totalPrice);
