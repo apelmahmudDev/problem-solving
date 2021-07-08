0d68cdbaa10ce696eba5a30708132a17f13f9a09
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-07-08 10:41:21 +0600

    problem-7 'hotel cost' is solved

--- second-assignment.js
+++ second-assignment.js
@@ -9,7 +9,7 @@ const kilometer = 30;
 const meter = kilometerToMeter(kilometer);
 // console.log(meter);
 
-// 6. budgetCalculator
+// 6. budget Calculator
 
 function budgetCalculator(watch, mobile, latptop) {
 	const watchPrice = 50;
@@ -24,4 +24,33 @@ const watch = 2;
 const mobile = 5;
 const latptop = 2;
 const totalPrice = budgetCalculator(watch, mobile, latptop);
-console.log(totalPrice);
+// console.log(totalPrice);
+
+// 7. hotel cost
+
+function hotelCost(placedCount) {
+	let totalHotelCost;
+	const first10DaysCost = 1000;
+	const second10Dayscost = 800;
+
+	if (placedCount <= 10) {
+		// for every night hotel cost = 100 taka
+		const hotelCost = 100;
+		totalHotelCost = placedCount * hotelCost;
+	} else if (placedCount <= 20) {
+		// for every night hotel cost = 80 taka
+		const hotelCost = 80;
+		totalHotelCost = (placedCount - 10) * hotelCost + first10DaysCost;
+	} else {
+		// for every night hotel cost = 50 taka
+		const hotelCost = 50;
+		totalHotelCost =
+			(placedCount - 20) * hotelCost + first10DaysCost + second10Dayscost;
+	}
+
+	return totalHotelCost;
+}
+
+const placedCount = 30; // how many days placed in hotel
+const guestHotelCost = hotelCost(placedCount);
+console.log(guestHotelCost); // your total hotel cost
